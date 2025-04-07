65df247d6dc69151b7e611bb69d819fa7ffb92e4
RithyTep <rithy.tep@techbodia.com>
2025-04-07 16:41:03 +0700

    [IntegrateManagementDrink][SP] Add: AccountType & Remove unused code

--- dbo/Store Procedures/MercyCafe_Main_AdminLogin.sql
+++ dbo/Store Procedures/MercyCafe_Main_AdminLogin.sql
@@ -1,29 +1,29 @@
 ﻿CREATE PROCEDURE [dbo].[MercyCafe_Main_CustomerLogin_1.0.0]
-    @UserName NVARCHAR(50) = '',
-    @Password NVARCHAR(255) = ''
+  @UserName NVARCHAR(50) = '',
+  @Password NVARCHAR(255) = '',
+  @AccountType TINYINT = 1
 AS
 BEGIN
-    DECLARE @StoredPassword NVARCHAR(255);
+  DECLARE @StoredPassword NVARCHAR(255);
 
-    SELECT @StoredPassword = Password
-    FROM [dbo].[Customer] WITH (NOLOCK)
-    WHERE UserName = @UserName;
+  SELECT @StoredPassword = Password
+  FROM [dbo].[Customer] WITH (NOLOCK)
+  WHERE UserName = @UserName
+  AND AccountType = @AccountType;
 
-    IF @StoredPassword IS NOT NULL
+  IF @StoredPassword IS NOT NULL
+  BEGIN
+    IF @Password = @StoredPassword
     BEGIN
-        IF @Password = @StoredPassword
-        BEGIN
-            SELECT 0 AS ErrorCode, 'success' AS ErrorMessage;`
-        END
-        ELSE
-        BEGIN
-            SELECT -1 AS ErrorCode, 'password incorrect' AS ErrorMessage;
-        END
+      SELECT 0 AS ErrorCode, 'success' AS ErrorMessage;
     END
     ELSE
     BEGIN
-        SELECT -1 AS ErrorCode, 'customer not found' AS ErrorMessage;
+      SELECT -1 AS ErrorCode, 'password incorrect' AS ErrorMessage;
     END
-
-    SELECT 0 AS ErrorCode, 'success' AS ErrorMessage;
+  END
+  ELSE
+  BEGIN
+    SELECT -1 AS ErrorCode, 'customer not found' AS ErrorMessage;
+  END
 END
